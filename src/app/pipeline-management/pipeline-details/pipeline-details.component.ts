98af5136e4bd02d633a18d6ed212a2203240a4ac
yerramshilpa <44271677+yerramshilpa@users.noreply.github.com>
2020-10-30 09:45:16 -0400

    expand all

--- src/app/pipeline-management/pipeline-details/pipeline-details.component.ts
+++ src/app/pipeline-management/pipeline-details/pipeline-details.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import * as _ from 'lodash';
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import * as _ from "lodash";
 
 // Models
 
 // Shared
-import { expansionIndicatorRotate } from '../../multifamily/shared/components/animate-animation'
+import { expansionIndicatorRotate } from "../../multifamily/shared/components/animate-animation";
 //'ultifamily/shared/components';
 
 // Metadata
@@ -13,15 +13,18 @@ import {
   DEFAULT_PIPELINE_COLUMN_METADATA,
   QUOTE_RESULTS_APPROVAL_COLUMN_METADATA,
   QUOTE_RESULTS_COLUMN_METADATA,
-  QUOTE_RESULTS_EXTENSION_COLUMN_METADATA
-} from './pipeline.constant';
-import { PipelineManagementService } from '../../services/pipeline-management.service'
-import { Observable } from 'rxjs/internal/Observable';
-import { Observer } from 'rxjs';
-import { MatTabChangeEvent } from '@angular/material/tabs';
-import { TableOptions, TableColumn } from '../../shared/components/table/model/table.model';
-import { TableComponent } from '../../shared/components/table/table/table.component';
-import { MOCK_PIPELINE_RESULTS } from '../../mocks/pipeline-management.mocks';
+  QUOTE_RESULTS_EXTENSION_COLUMN_METADATA,
+} from "./pipeline.constant";
+import { PipelineManagementService } from "../../services/pipeline-management.service";
+import { Observable } from "rxjs/internal/Observable";
+import { Observer } from "rxjs";
+import { MatTabChangeEvent } from "@angular/material/tabs";
+import {
+  TableOptions,
+  TableColumn,
+} from "../../shared/components/table/model/table.model";
+import { TableComponent } from "../../shared/components/table/table/table.component";
+import { MOCK_PIPELINE_RESULTS } from "../../mocks/pipeline-management.mocks";
 
 export interface PipelineTab {
   label?: string;
@@ -33,25 +36,29 @@ export interface PipelineTab {
 }
 
 @Component({
-  selector: 'mf-pe-pipeline-details',
-  templateUrl: './pipeline-details.component.html',
-  styleUrls: ['./pipeline-details.component.scss'],
-  animations: [expansionIndicatorRotate]
+  selector: "mf-pe-pipeline-details",
+  templateUrl: "./pipeline-details.component.html",
+  styleUrls: ["./pipeline-details.component.scss"],
+  animations: [expansionIndicatorRotate],
 })
 export class PipelineDetailsComponent implements OnInit {
-  title = 'Welcome';
+  title = "Welcome";
   quotes: any = {
     newQuotes: [],
     extensionQuotes: [],
     awaitingApprovalQuotes: [],
-    results: null
+    results: null,
   };
 
   defaultColMeta: any[] = _.clone(DEFAULT_PIPELINE_COLUMN_METADATA);
   approvalColMeta: any[] = _.clone(AwaitingApprovalData);
   quoteResultsColMeta: any[] = _.clone(QUOTE_RESULTS_COLUMN_METADATA);
-  quoteResultsExtensionColMeta: any[] = _.clone(QUOTE_RESULTS_EXTENSION_COLUMN_METADATA);
-  quoteResultsApprovalColMeta: any[] = _.clone(QUOTE_RESULTS_APPROVAL_COLUMN_METADATA);
+  quoteResultsExtensionColMeta: any[] = _.clone(
+    QUOTE_RESULTS_EXTENSION_COLUMN_METADATA
+  );
+  quoteResultsApprovalColMeta: any[] = _.clone(
+    QUOTE_RESULTS_APPROVAL_COLUMN_METADATA
+  );
   quoteResultsExtensionTableOptions: TableOptions;
   quoteResultsApprovalTableOptions: TableOptions;
 
@@ -60,15 +67,26 @@ export class PipelineDetailsComponent implements OnInit {
   defaultTableOptions: TableOptions;
   quoteResultsTableOptions: TableOptions;
   approvalTableOptions: TableOptions;
-  @ViewChild('tabGroup', { static: false }) tabGroup: any;
+  @ViewChild("pipelineResultsTable") pipelineResultsTableRef: TableComponent;
   @ViewChild(TableComponent, { static: false }) tableComponent: TableComponent;
+  expandedElement: any | null;
+  allRowsExpanded: boolean = false;
 
   // ------------------------------ Init ------------------------------
 
-  constructor(private pipelineService: PipelineManagementService) { }
+  constructor(private pipelineService: PipelineManagementService) {}
+
+  public toggle() {
+    this.allRowsExpanded = !this.allRowsExpanded;
+    // this.pipelineResultsTableRef.nativeElement.expandedRow = null;
+    console.log(
+      this.pipelineResultsTableRef,
+      "ref",
+      this.tableComponent.expandedRow
+    );
+  }
 
   ngOnInit() {
-
     this.defaultTableOptions = Object.assign(
       {},
       _.cloneDeep(this.getTableOptions(this.defaultColMeta)),
@@ -97,19 +115,17 @@ export class PipelineDetailsComponent implements OnInit {
   getQuoteCount(items: any) {
     let count = 0;
     if (items.length > 0) {
-      items.forEach(item => {
-        count = count + item.quoteResults.length
+      items.forEach((item) => {
+        count = count + item.quoteResults.length;
       });
-
     }
     return count;
   }
 
-
   // ------------------------------ Table ------------------------------
   getQuotes(data) {
     const quoteResults = _.map(data, (item: any) => {
-      item.expandedMasterDetail = _.get(item, 'quoteResults', []);
+      item.expandedMasterDetail = _.get(item, "quoteResults", []);
       return item;
     });
     return quoteResults;
@@ -123,29 +139,29 @@ export class PipelineDetailsComponent implements OnInit {
     this.asyncTabs = new Observable((observer: Observer<PipelineTab[]>) => {
       observer.next([
         {
-          label: 'New',
+          label: "New",
           content: this.getQuotes(this.quotes.newQuotes),
           index: 0,
           tableOptions: this.defaultTableOptions,
           quoteTableOptions: this.quoteResultsTableOptions,
-          quoteCount: this.getQuoteCount(this.quotes.newQuotes)
+          quoteCount: this.getQuoteCount(this.quotes.newQuotes),
         },
         {
-          label: 'Extension',
+          label: "Extension",
           content: this.getQuotes(this.quotes.extensionQuotes),
           index: 1,
           tableOptions: this.defaultTableOptions,
           quoteTableOptions: this.quoteResultsExtensionTableOptions,
-          quoteCount: this.getQuoteCount(this.quotes.extensionQuotes)
+          quoteCount: this.getQuoteCount(this.quotes.extensionQuotes),
         },
         {
-          label: 'Awaiting Approval',
+          label: "Awaiting Approval",
           content: this.getQuotes(this.quotes.awaitingApprovalQuotes),
           index: 2,
           tableOptions: this.approvalTableOptions,
           quoteTableOptions: this.quoteResultsApprovalTableOptions,
-          quoteCount: this.getQuoteCount(this.quotes.awaitingApprovalQuotes)
-        }
+          quoteCount: this.getQuoteCount(this.quotes.awaitingApprovalQuotes),
+        },
       ]);
       this.lastUpdated = new Date();
     });
@@ -155,7 +171,10 @@ export class PipelineDetailsComponent implements OnInit {
   // This will handle setting up the table options
   getTableOptions(pipelineColMetadata: any[], isQuoteOptions: boolean = false) {
     if (!_.isEmpty(pipelineColMetadata)) {
-      const options: TableOptions = this._mapPipelineOptions(pipelineColMetadata, isQuoteOptions);
+      const options: TableOptions = this._mapPipelineOptions(
+        pipelineColMetadata,
+        isQuoteOptions
+      );
       return options;
     }
   }
@@ -169,7 +188,7 @@ export class PipelineDetailsComponent implements OnInit {
       columns: columns,
       elevatedTable: false,
       flexColumns: true,
-      rowHeightStyle: 'highly-condensed'
+      rowHeightStyle: "highly-condensed",
     };
 
     return options;
@@ -178,7 +197,7 @@ export class PipelineDetailsComponent implements OnInit {
   navigateTo(quoteId: number) {
     const origin: string = location.origin;
     const url = `${origin}/pe/quote/${quoteId}`;
-    window.open(url, '_blank');
+    window.open(url, "_blank");
   }
 
   private mapQuoteResultsColumn(colMetadata: any[]): TableColumn[] {
@@ -187,26 +206,26 @@ export class PipelineDetailsComponent implements OnInit {
         key: dc.dataMappingName,
         displayName: dc.name,
         hasCellTemplate:
-          dc.dataMappingName === 'opportunityUpbAmt' ||
-          dc.dataMappingName === 'statusType' ||
-          dc.dataMappingName === 'quoteId' ||
-          dc.dataMappingName === 'priorityIndicator' ||
-          dc.dataMappingName === 'submitDateTime',
+          dc.dataMappingName === "opportunityUpbAmt" ||
+          dc.dataMappingName === "statusType" ||
+          dc.dataMappingName === "quoteId" ||
+          dc.dataMappingName === "priorityIndicator" ||
+          dc.dataMappingName === "submitDateTime",
         hasEditableCells: dc.dataValueEditableInd,
         cellValueType: dc.dataValueType,
 
         cellTemplateClassName:
-          dc.dataMappingName === 'quoteId'
-            ? 'mf-w-10'
-            : dc.dataMappingName === 'opportunityUpbAmt'
-              ? 'mf-w-7'
-              : dc.dataMappingName === 'statusType'
-                ? 'mf-text-input-cell mf-w-12'
-                : dc.dataMappingName === 'quoteType'
-                  ? 'mf-w-15'
-                  : dc.dataMappingName === 'submitDateTime'
-                    ? 'mf-w-10'
-                    : ''
+          dc.dataMappingName === "quoteId"
+            ? "mf-w-10"
+            : dc.dataMappingName === "opportunityUpbAmt"
+            ? "mf-w-7"
+            : dc.dataMappingName === "statusType"
+            ? "mf-text-input-cell mf-w-12"
+            : dc.dataMappingName === "quoteType"
+            ? "mf-w-15"
+            : dc.dataMappingName === "submitDateTime"
+            ? "mf-w-10"
+            : "",
       };
     });
     return columns;
@@ -219,27 +238,27 @@ export class PipelineDetailsComponent implements OnInit {
         key: dc.dataMappingName,
         displayName: _.toUpper(dc.name),
         hasCellTemplate:
-          dc.dataMappingName === 'quoteCount' ||
-          dc.dataMappingName === 'maxUPB' ||
-          dc.dataMappingName === 'opportunityId' ||
-          dc.dataMappingName === 'submitDateTime',
+          dc.dataMappingName === "quoteCount" ||
+          dc.dataMappingName === "maxUPB" ||
+          dc.dataMappingName === "opportunityId" ||
+          dc.dataMappingName === "submitDateTime",
         hasEditableCells: dc.dataValueEditableInd,
         cellValueType: dc.dataValueType,
 
         cellTemplateClassName:
-          dc.dataMappingName === 'borrower'
-            ? 'mf-text-input-cell mf-w-15'
-            : dc.dataMappingName === 'propertyName'
-              ? 'mf-w-15'
-              : dc.dataMappingName === 'opportunityId'
-                ? 'mf-w-15'
-                : dc.dataMappingName === 'maxUPB'
-                  ? 'mf-w-7'
-                  : dc.dataMappingName === 'quoteCount'
-                    ? 'mf-w-7'
-                    : dc.dataMappingName === 'submitDateTime'
-                      ? 'mf-w-10'
-                      : ''
+          dc.dataMappingName === "borrower"
+            ? "mf-text-input-cell mf-w-15"
+            : dc.dataMappingName === "propertyName"
+            ? "mf-w-15"
+            : dc.dataMappingName === "opportunityId"
+            ? "mf-w-15"
+            : dc.dataMappingName === "maxUPB"
+            ? "mf-w-7"
+            : dc.dataMappingName === "quoteCount"
+            ? "mf-w-7"
+            : dc.dataMappingName === "submitDateTime"
+            ? "mf-w-10"
+            : "",
       };
     });
     return columns;
